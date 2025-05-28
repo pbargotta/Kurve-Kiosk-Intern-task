bee474f61628f259a9cafb94ef9e50f516b0a603
Pavan Bargotta <89701532+pbargotta@users.noreply.github.com>
2025-05-28 21:26:03 +0100

    Remove unnecessary files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="mb-4 text-4xl font-bold text-blue-600">
         Kurve Kiosk Customers
       </h1>
       <p className="text-lg text-gray-700">
